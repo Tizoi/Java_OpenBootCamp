f3f2fd0bc1a7eeb3f87653be9be5e33206dc4964
Abel <abelboy94@gmail.com>
2022-09-29 22:04:35 +0200

    Proyecto creado

---
+++ JavaCode/src/documentacion/Main.java
@@ -0,0 +1,25 @@
+package documentacion;
+
+public class Main {
+
+    /**
+     * Función principal punto de entrada para la aplicación
+     *
+     * @param args argumentos a recibir
+     * @since 1.0
+     */
+    public static void moin (String[] args){
+        //codigo
+
+        /*
+        *
+        * más comentarios
+        *
+        * */
+
+        /*
+        sadad
+         */
+
+    }
+}
